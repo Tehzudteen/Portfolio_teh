ae434a05a234d3f1ddb969e4e77b02331fe747c5
Tehzudteen <luckyteh0083@gmail.com>
2025-05-22 14:15:53 +0700

    SEVEN

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-// next.config.mjs
 const nextConfig = {
+  output: 'export',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
 }
 
 export default nextConfig
-
